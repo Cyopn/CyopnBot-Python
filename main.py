5486ceed7559c2ff05e524c0e26cd6f72120f145
Cyopn <frijolesconcatsu@gmail.com>
2022-03-01 23:23:13 -0600

    Clase principal

---
+++ main.py
@@ -0,0 +1,30 @@
+from discord import Intents, Game, Status
+from discord.ext import commands
+import os
+import keep
+
+i=Intents.all()
+
+client=commands.Bot(command_prefix='+', intents=i)
+
+@client.event
+async def on_ready():
+    print(f'Listo \nIniciado en {client.user}')
+    
+    """ e=[1000, 1000*500] """
+    g=Game(name='+help | Hola', extra=[1000, 1000*5000])
+    await client.change_presence(activity=g, status=Status.dnd)
+    
+@client.event
+async def on_message(msg):
+    if msg.author ==client.user or msg.author.bot:
+        return
+    await client.process_commands(msg)
+
+client.load_extension(name='cogs.fun')
+client.load_extension(name='cogs.music')
+client.load_extension(name='cogs.guild')
+
+keep.keep_alive()
+    
+client.run(os.environ['token'])
