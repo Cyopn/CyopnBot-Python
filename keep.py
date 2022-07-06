d201b1af17c5682df9d9616fd0144098701fc57d
Cyopn <frijolesconcatsu@gmail.com>
2022-07-06 00:53:48 -0500

    Mantiene en linea

--- keep.py
+++ keep.py
@@ -1,15 +1,15 @@
-from flask import Flask
+""" from flask import Flask, render_template
 from threading import Thread
 
 app = Flask('Cyopn')
 
 @app.route('/')
-def home():
-    return "Servidor listo"
+def index():
+    return render_template("index.html")
 
 def run():
   app.run(host='0.0.0.0',port=8080)
 
 def keep_alive():  
     t = Thread(target=run)
-    t.start()
+    t.start() """
